35f2bcb481fe4323e79f86a627f2e3339d8d815a
Ayush <ayush.mohjay@gmail.com>
2026-06-13 22:35:11 +0530

    updates

--- includes/config.php
+++ includes/config.php
@@ -1,7 +1,9 @@
 <?php
 
 // Base URL
-define('BASE_URL', 'https://miskills.in');
+// define('BASE_URL', 'http://localhost/mi-skillls-website/');
+define('BASE_URL', 'https://miskills.in/');
+
 
 // Backend API URL
 define('API_BASE_URL', 'https://dev.miskills.in');
